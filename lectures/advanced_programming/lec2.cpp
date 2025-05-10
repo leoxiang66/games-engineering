ff944c93cea856c86be65a65dd9db1b18d5acc01
Leon <xiang.tao@outlook.de>
2025-05-10 16:22:06 +0800

    Create lec2.cpp

---
+++ lectures/advanced_programming/lec2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+const double pi = 3.1415;
+
+int main(int argc, char const *argv[])
+{
+    float a{static_cast<float>(39.123 + pi)};
+    int b;
+
+    std::cout << "a=" << a << " b = " << b << std::endl;
+    b = a;
+    std::string message{"The answer to everything is "};
+    std::cout << message << std::to_string(b) << std::endl;
+
+    // how to initialize basic data types
+    // avoid
+    int b = 0; // classic way: var = exp
+    int b(0);  // classic way: var(expression list); basic types: =, classes: constructor
+
+    // more preferred: 因為和複雜的type的init一樣
+    int b{0}; // since c++11
+    int b{};  // since c++11: defauts to 0 for int
+    return 0;
+}
